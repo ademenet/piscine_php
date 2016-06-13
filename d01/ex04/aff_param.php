3ba8e489478c2ede6781be54e1113415942effe3
Alain <ademenet@student.42.fr>
2016-06-13 08:15:43 +0200

    wip

--- d01/ex04/aff_param.php
+++ d01/ex04/aff_param.php
@@ -1,7 +1,9 @@
 #!/usr/bin/php
 <?php
 	if ($argc > 1) {
-		foreach (array_slice($argv, 1) as $arg)
-			echo $arg . "\n";
+		if (isset($argv)) {
+			foreach (array_slice($argv, 1) as $arg)
+				echo $arg . "\n";
+		}
 	}
  ?>
