43e1082c6af71a3d967bee8e0edb002287e45a5a
Alain DEMENET <ademenet@student.42.fr>
2016-06-14 19:39:37 +0200

    Correcting sort iin both ways

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -1,10 +1,15 @@
 <?php
-// il faut utiliser casesort et voir s'il est trie dans un sens ou dans l'autre
 	function ft_is_sort($array) {
 		if (isset($array) && is_array($array)) {
-			$sorted = $default = array_values($array);
+			$flag = false;
+			$sorted = $rsorted = $default = array_values($array);
 			sort($sorted);
-			return $default === $sorted;
+			rsort($rsorted);
+			if ($default === $sorted)
+				$flag = true;
+			elseif ($default === $rsorted)
+				$flag = true;
+			return $flag;
 		}
 	}
  ?>
