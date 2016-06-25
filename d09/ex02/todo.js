fdf41ba548560a9e29430f3dc06ef4603f1916cd
Alain <ademenet@student.42.fr>
2016-06-25 13:37:23 +0200

    wip

--- d09/ex02/todo.js
+++ d09/ex02/todo.js
@@ -1,20 +1,51 @@
+var listItem = [];
+
+// 1 j affiche le contenu de mon cookie
+console.log(document.cookie);
+// display list items
+if (document.cookie) {
+	listItem = JSON.parse(document.cookie);
+	console.log(listItem);
+	for(var i = 0; i < listItem.length; i++) {
+		var list = document.getElementById('ft_list');
+		var lastItem = document.createElement('div');
+		lastItem.innerHTML = listItem[i];
+		lastItem.insertBefore(lastItem, list.firstChild);
+		// lastItem.onclick = eraseToDo();
+	}
+}
+
+// todoChild.innerHTML = tab[elem];
+// todo.insertBefore(todoChild, todo.firstChild);
+// document.getElementById(elem).addEventListener('click', function(){
+// 	ft_remove(this.id);
+// }, false);
+
+
+
 function addToDo() {
 	var toDo = prompt("Veuillez rentrer une tâche :");
 	if (toDo != "") {
-		var toDos = getCookie('listItem');
-		console.log(toDos);
-		if (toDos) {
+		var list = document.getElementById('ft_list');
+		var newItem = document.createElement('div');
+		newItem.setAttribute("id", "todo");
+		newItem.innerHTML = toDo;
+		list.insertBefore(newItem, list.firstChild);
+		listItem.push(toDo);
+		document.cookie = createCookie('todo', JSON.stringify(listItem), 1);
+		lastItem.onclick = eraseToDo(newItem);
+		console.log(document.cookie);
+	}
+}
 
-		} else {
-			createCookie('listItem', toDo, 10);
-		}
+function eraseToDo() {
+	if (confirm('Voulez-vous supprimer cette tâche ?')) {
+
 	}
 }
 
 function createCookie(name, value, days) {
-	alert(name);
-	alert(value);
-	alert(days);
+	console.log(value);
 	if (days) {
 		var date = new Date();
 		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
@@ -26,25 +57,24 @@ function createCookie(name, value, days) {
 	var str = name + "=" + value + expires;
 	console.log(str);
 	document.cookie = str;
-	console.log(document.cookie);
 }
 
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ')
-			c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0)
-			return c.substring(nameEQ.length, c.length);
-	}
-	return null;
-}
+// function getCookie(name) {
+// 	var nameEQ = name + "=";
+// 	var ca = document.cookie.split(';');
+// 	for(var i = 0; i < ca.length; i++) {
+// 		var c = ca[i];
+// 		while (c.charAt(0)==' ')
+// 			c = c.substring(1, c.length);
+// 		if (c.indexOf(nameEQ) == 0)
+// 			return c.substring(nameEQ.length, c.length);
+// 	}
+// 	return null;
+// }
 
-function eraseCookie(name) {
-	createCookie(name, "", -1);
-}
+// function eraseCookie(name) {
+// 	createCookie(name, "", -1);
+// }
 
 
 // Specify them all once eg:
